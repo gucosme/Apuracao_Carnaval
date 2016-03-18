0ca7bbfe2a6f984ff74b7c1153894704c8cb2660
Gustavo C <gustavocosme@outlook.com>
2016-03-17 23:33:13 -0300

    Adição entidades e GenericDAO

---
+++ src/entity/Notas.java
@@ -0,0 +1,59 @@
+package entity;
+
+public class Notas {
+
+	private int idEscola;
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+	private double nota5;
+	private double totaPontos;
+	
+	public int getIdEscola() {
+		return idEscola;
+	}
+	public void setIdEscola(int idEscola) {
+		this.idEscola = idEscola;
+	}
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	public double getNota3() {
+		return nota3;
+	}
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+	public double getNota4() {
+		return nota4;
+	}
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+	public double getNota5() {
+		return nota5;
+	}
+	public void setNota5(double nota5) {
+		this.nota5 = nota5;
+	}
+	public double getTotaPontos() {
+		return totaPontos;
+	}
+	public void setTotaPontos(double totaPontos) {
+		this.totaPontos = totaPontos;
+	}
+	
+	
+	
+	
+}
